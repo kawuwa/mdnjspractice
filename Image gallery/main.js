03c6d107ddf1279ccf7bb5a05f31f2774f916f05
kawuwa <3066839538@qq.com>
2023-03-28 22:31:33 +0800

    english

--- Image gallery/main.js
+++ Image gallery/main.js
@@ -8,6 +8,7 @@ const overlay = document.querySelector('.overlay');
 let arr=['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 /* Declaring the alternative text for each image file */
 let textpic=['1','2','3','4','5'];
+
 /* Looping through images */
 for (let i=0;i<5;i++) {
 	const newImage = document.createElement('img');
@@ -15,6 +16,7 @@ for (let i=0;i<5;i++) {
 	newImage.setAttribute('alt',textpic[i] );
 	thumbBar.appendChild(newImage);
 }
+
 /* Wiring up the Darken/Lighten button */
 console.log(overlay.style.backgroundColor);
 function dl(){
@@ -24,8 +26,8 @@ function dl(){
 		overlay.style.backgroundColor = 'rgba(0,0,0,0)';
 	}
 }
-//overlay.style.backgroundColor 这个返回的字符串里有空格！
-//颜色怎么设置？
+//notice!!! It's 'rgba(0, 0, 0, 0)' but not 'rgba(0,0,0,0)'.
+
 btn.addEventListener('click',dl);
 
 thumbBar.addEventListener('click',function(e) {
